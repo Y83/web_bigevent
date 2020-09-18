f137d013cefeee258f908053b1259c0357bb6201
Z <934408304@qq.com>
2020-09-18 14:49:59 +0800

    完成文章管理相关功能的开发

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,110 @@
+$(function () {
+    initArtCateList()
+
+    // 获取文章分类的列表
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+
+    var layer = layui.layer
+    var indexAdd = null
+    // 为添加类别按钮绑定点击事件
+    $('#btnAddCate').on('click', function () {
+        indexAdd = layer.open({ // 每一种弹层调用方式，都会返回一个index
+            type: 1,
+            area: ['500px', '260px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        });
+    })
+
+    // 通过代理的形式为 form-add 表单绑定 submit 事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！')
+                }
+                initArtCateList()
+                layer.msg('新增分类成功！')
+                // 根据索引关闭对应的弹出层
+                layer.close(indexAdd)
+            }
+        })
+    })
+
+    var indexEdit = null
+    // 通过代理的形式为 btn-edit 按钮绑定点击事件
+    $('tbody').on('click', '.btn-edit', function () {
+        // 弹出一个修改文章分类信息的层
+        indexEdit = layer.open({ // 每一种弹层调用方式，都会返回一个index
+            type: 1,
+            area: ['500px', '260px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        });
+
+        var id = $(this).attr('data-id')
+        var form = layui.form
+        // console.log(id);
+        // 发起请求获取对应分类的数据
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/' + id,
+            success: function (res) {
+                // console.log(res);
+                form.val('form-edit', res.data)
+            }
+        })
+    })
+
+    // 通过代理的形式，为修改分类的表单绑定 submit 事件
+    $('body').on('submit', '#form-edit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status === 0) {
+                    layer.close(indexEdit)
+                    initArtCateList()
+                }
+                layer.msg(res.message)
+            }
+        })
+    })
+
+    // 通过代理的形式，为删除按钮绑定点击事件
+    $('tbody').on('click', '.btn-delete', function () {
+        var id = $(this).attr('data-id')
+        // 提示用户是否要删除
+        layer.confirm('确定删除?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function (res) {
+                    if (res.status === 0) {
+                        layer.close(index)
+                        initArtCateList()
+                    }
+                    layer.msg(res.message)
+                }
+            })
+        });
+    })
+})
